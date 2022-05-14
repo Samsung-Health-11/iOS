4817da61d13f4118477cb8d20fd5bb64283ff601
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-05-14 16:23:42 +0900

    [#1] add info dependencies

---
+++ SamsungHealth-iOS/SamsungHealth-iOS/Helpers/AppExtensions/ViewController+.swift
@@ -0,0 +1,8 @@
+//
+//  ViewController+.swift
+//  SamsungHealth-iOS
+//
+//  Created by 이경민 on 2022/05/14.
+//
+
+import Foundation
---
+++ SamsungHealth-iOS/SamsungHealth-iOS/Scene(baby)/Weight/Cell/BabyDummyWeightCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  BabyDummyWeightCollectionViewCell.swift
+//  SamsungHealth-iOS
+//
+//  Created by 이경민 on 2022/05/14.
+//
+
+import UIKit
+
+class BabyDummyWeightCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}

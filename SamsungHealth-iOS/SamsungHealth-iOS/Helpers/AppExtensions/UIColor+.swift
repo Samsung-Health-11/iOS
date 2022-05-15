5141caa4ba682161c6b972c8965327cc9e7ace59
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-05-15 13:24:08 +0900

    [#1] add colors

---
+++ SamsungHealth-iOS/SamsungHealth-iOS/Helpers/AppExtensions/UIColor+.swift
@@ -0,0 +1,88 @@
+//
+//  UIColor+.swift
+//  SamsungHealth-iOS
+//
+//  Created by 이경민 on 2022/05/15.
+//
+
+import UIKit
+
+extension UIColor {
+
+  @nonobjc class var SshColorBlack1: UIColor {
+    return UIColor(white: 37.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var SshColorGray1: UIColor {
+    return UIColor(white: 118.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var SshColorBlack2: UIColor {
+    return UIColor(white: 67.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var SshColorGray2: UIColor {
+    return UIColor(white: 138.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var SshColorGray3: UIColor {
+    return UIColor(white: 155.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var SshColorGray4: UIColor {
+    return UIColor(white: 192.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var SshColorGray5: UIColor {
+    return UIColor(white: 230.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var SshColorGray6: UIColor {
+    return UIColor(white: 245.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var SshColorGray7: UIColor {
+    return UIColor(white: 253.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var SshColorBgwhite: UIColor {
+    return UIColor(white: 252.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var SshColorBggray: UIColor {
+    return UIColor(white: 246.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var SshColorPink: UIColor {
+    return UIColor(red: 1.0, green: 57.0 / 255.0, blue: 160.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var SshColorPinklight: UIColor {
+    return UIColor(red: 252.0 / 255.0, green: 228.0 / 255.0, blue: 241.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var SshColorGreen: UIColor {
+    return UIColor(red: 16.0 / 255.0, green: 207.0 / 255.0, blue: 111.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var SshColorGreenlight: UIColor {
+    return UIColor(red: 207.0 / 255.0, green: 249.0 / 255.0, blue: 229.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var SshColorSkyblue: UIColor {
+    return UIColor(red: 77.0 / 255.0, green: 169.0 / 255.0, blue: 1.0, alpha: 1.0)
+  }
+
+  @nonobjc class var SshColorSkybluelight: UIColor {
+    return UIColor(red: 227.0 / 255.0, green: 241.0 / 255.0, blue: 254.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var SshColorSleepblue: UIColor {
+    return UIColor(red: 96.0 / 255.0, green: 121.0 / 255.0, blue: 240.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var SshColorWhite: UIColor {
+    return UIColor(white: 1.0, alpha: 1.0)
+  }
+
+}
--- SamsungHealth-iOS/SamsungHealth-iOS/Helpers/AppExtensions/ViewController+.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ViewController+.swift
-//  SamsungHealth-iOS
-//
-//  Created by 이경민 on 2022/05/14.
-//
-
-import Foundation

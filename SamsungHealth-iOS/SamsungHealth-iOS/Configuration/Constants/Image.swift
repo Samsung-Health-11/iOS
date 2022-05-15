8c0b26084b688fd722386c188ac8c0b72f759241
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-05-15 13:41:50 +0900

    [#1] add asset + const

--- SamsungHealth-iOS/SamsungHealth-iOS/Configuration/Constants/Image.swift
+++ SamsungHealth-iOS/SamsungHealth-iOS/Configuration/Constants/Image.swift
@@ -6,8 +6,29 @@
 //
 
 import Foundation
+import UIKit
 
 extension Const {
     struct Image {
+        
+        // MARK: - Home
+        
+        static let sshActivityheart = UIImage(named: "sshActivityheart")!
+        static let sshButtonList = UIImage(named: "sshButtonList")!
+        static let sshButtonStretch = UIImage(named: "sshButtonStretch")!
+        static let sshButtonWalk = UIImage(named: "sshButtonWalk")!
+        static let sshButtonYoga = UIImage(named: "sshButtonYoga")!
+        static let sshIconKcal = UIImage(named: "sshIconKcal")!
+        static let sshIconScale = UIImage(named: "sshIconScale")!
+        static let sshIconSteps = UIImage(named: "sshIconSteps")!
+        static let sshIconTime = UIImage(named: "sshIconTime")!
+        static let sshKnoxlogo = UIImage(named: "sshKnoxlogo")!
+        static let sshLogo = UIImage(named: "sshLogo")!
+        static let sshMemo = UIImage(named: "sshMemo")!
+        static let sshMenubutton = UIImage(named: "sshMenubutton")!
+        static let sshMinusButtonOff = UIImage(named: "sshMinusButtonOff")!
+        static let sshPlusButtonOn = UIImage(named: "sshPlusButtonOn")!
+        static let sshWalkbar = UIImage(named: "sshWalkbar")!
+        static let sshWritebutton = UIImage(named: "sshWritebutton")!
     }
 }

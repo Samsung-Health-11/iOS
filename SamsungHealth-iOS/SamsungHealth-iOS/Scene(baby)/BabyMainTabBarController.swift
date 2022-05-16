afa2280d5f81bb2281692da250b009f89ddbb430
Choi YoungLyn <joeuni0129@gmail.com>
2022-05-17 01:00:10 +0900

    [#3]feat Custom TabBar 구현

--- SamsungHealth-iOS/SamsungHealth-iOS/Scene(baby)/BabyMainTabBarController.swift
+++ SamsungHealth-iOS/SamsungHealth-iOS/Scene(baby)/BabyMainTabBarController.swift
@@ -9,21 +9,16 @@ import UIKit
 
 class BabyMainTabBarController: UITabBarController {
 
+    // MARK: - VC Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setTabBarController()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Function
+    func setTabBarController(){
+        tabBar.unselectedItemTintColor = UIColor(displayP3Red: 155/255, green: 155/255, blue: 155/255, alpha: 1)
+        tabBar.tintColor = UIColor(displayP3Red: 155/255, green: 155/255, blue: 155/255, alpha: 1)
     }
-    */
 
 }

ab3d5262f01670e22b93546cd5d333ad26046c7f
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-05-20 00:39:12 +0900

    [#9] Weight View UI

--- SamsungHealth-iOS/SamsungHealth-iOS/Scene(km)/Weight/KmWeightViewController.swift
+++ SamsungHealth-iOS/SamsungHealth-iOS/Scene(km)/Weight/KmWeightViewController.swift
@@ -7,23 +7,62 @@
 
 import UIKit
 
-class KmWeightViewController: UIViewController {
-
+final class KmWeightViewController: UIViewController {
+    
+    // MARK: - IBOutlet
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    @IBOutlet var componentViewCollection: [UIView]!
+    @IBOutlet var titleLabelCollection: [UILabel]!
+    @IBOutlet var explainLabelCollection: [UILabel]!
+    
+    @IBOutlet weak var buttonBackgroundViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet var buttonWidthConstraintCollection: [NSLayoutConstraint]!
+    
+    // MARK: - VC Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Functions
+    
+    private func setUI() {
+        setViewUI()
+        setLabelUI()
+        setButtonUI()
     }
-    */
+    
+    // MARK: - IBAction
+    
+    @IBAction func cancelButtonDidTap(_ sender: Any) {
+    }
+    
+    @IBAction func saveButtonDidTap(_ sender: Any) {
+    }
+}
 
+extension KmWeightViewController {
+    private func setViewUI() {
+        componentViewCollection.forEach { $0.layer.cornerRadius = 20 }
+    }
+    
+    private func setLabelUI() {
+        titleLabel.font = .SshFontH2
+        timeLabel.font = .SshFontB1
+        titleLabelCollection.forEach { $0.font = .NotoSans(.medium, size: 15) }
+        explainLabelCollection.forEach { $0.font = .SshFontB3 }
+    }
+    
+    private func setButtonUI() {
+        buttonWidthConstraintCollection.forEach { $0.constant = UIScreen.main.bounds.width / 2 }
+        [cancelButton, saveButton].forEach {
+            $0?.titleLabel?.font = .SshFontB1
+        }
+    }
 }

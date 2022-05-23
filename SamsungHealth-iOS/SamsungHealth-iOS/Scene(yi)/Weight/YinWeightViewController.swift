9202ffcc10c1e08c6d36c0ae4ab6dec803f7b2ae
김영인 <0inn1220@gmail.com>
2022-05-23 20:19:22 +0900

    [#7] WeightView 구현
    화면 구성 및 pickerView custom 완료

--- SamsungHealth-iOS/SamsungHealth-iOS/Scene(yi)/Weight/YinWeightViewController.swift
+++ SamsungHealth-iOS/SamsungHealth-iOS/Scene(yi)/Weight/YinWeightViewController.swift
@@ -8,22 +8,107 @@
 import UIKit
 
 class YinWeightViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    var weight = Float(0.0)
+    
+    // MARK: - IBOutlet
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet var msgLabels: [UILabel]!
+    @IBOutlet var titleLabels: [UILabel]!
+    @IBOutlet var typeLabels: [UILabel]!
+    
+    @IBOutlet weak var weightPickerView: UIPickerView!
+    @IBOutlet weak var weightTextField: UITextField!
+    @IBOutlet weak var muscleTextField: UITextField!
+    
+    // MARK: - VC Life Cycle (or Cell Life Cycle)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewWillLayoutSubviews() {
+        initPickerView()
     }
-    */
+    
+    // MARK: - Function
+    
+    private func setUI() {
+        setFont()
+        setPickerView()
+        setTextField()
+    }
+    
+    private func setFont() {
+        titleLabel.font = .SshFontH2
+        dateLabel.font = .SshFontB1
+        msgLabels.forEach { $0.font = .SshFontB3 }
+        titleLabels.forEach { $0.font = .NotoSans(.medium, size: 15) }
+        typeLabels.forEach { $0.font = .NotoSans(.medium, size: 13)}
+    }
+    
+    private func initPickerView() {
+        weightPickerView.subviews[1].backgroundColor = .clear
+    }
+    
+    private func setPickerView() {
+        weightPickerView.delegate = self
+        weightPickerView.dataSource = self
+    }
+    
+    private func setTextField() {
+        [weightTextField, muscleTextField].forEach {
+            let bottomBorder = CALayer()
+            bottomBorder.borderWidth = 1
+            bottomBorder.borderColor = UIColor.SshColorGray3.cgColor
+            bottomBorder.frame = CGRect(x: 0, y: $0?.frame.height ?? 0, width: $0?.frame.width ?? 0, height: 1)
+            $0?.layer.addSublayer(bottomBorder)
+        }
+    }
+    
+    // MARK: - objc Function
+    // MARK: - IBAction
+    
+}
 
+extension YinWeightViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch (component) {
+        case 0: return 100
+        case 1: return 1
+        default: return 10
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 100, height: 60))
+        label.textAlignment = .center
+        label.font = UIFont.SshFontH1.withSize(24)
+        switch (component) {
+        case 1: label.text = "."
+        default: label.text = "\(row)"
+        }
+        return label
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 50
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        weight = Float(pickerView.selectedRow(inComponent: 0))
+        weight += (Float(pickerView.selectedRow(inComponent: 2)) * 0.1)
+    }
+    
 }

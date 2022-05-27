ada1309e4b162f9fe08ecc6a015710277a90b701
이경민 <74659491+meenyweeny@users.noreply.github.com>
2022-05-27 15:50:45 +0900

    [#17] Home UI

--- SamsungHealth-iOS/SamsungHealth-iOS/Scene(km)/Home/KmHomeViewController.swift
+++ SamsungHealth-iOS/SamsungHealth-iOS/Scene(km)/Home/KmHomeViewController.swift
@@ -11,18 +11,44 @@ final class KmHomeViewController: UIViewController {
     
     // MARK: - Property
     
+    var weight: Float?
     var waterCount: Int = 0
     
     // MARK: - IBOutlet
     
+    ///walk
+    @IBOutlet weak var walkCountLabel: UILabel!
+    @IBOutlet weak var walkTargetLabel: UILabel!
+    @IBOutlet weak var walkPercentLabel: UILabel!
+    
+    ///daily activity
+    @IBOutlet weak var activityWalkLabel: UILabel!
+    @IBOutlet weak var activityHourLabel: UILabel!
+    @IBOutlet weak var activityCalorieLabel: UILabel!
+    
+    ///food
+    @IBOutlet weak var foodIntakeLabel: UILabel!
+    @IBOutlet weak var foodTargetLabel: UILabel!
+    
+    ///sleep
+    @IBOutlet weak var sleepHourLabel: UILabel!
+    @IBOutlet weak var sleepMinuteLabel: UILabel!
+    @IBOutlet weak var sleepTimeLabel: UILabel!
     @IBOutlet weak var sleepBarView: UIView!
     
+    ///weight
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    ///water
     @IBOutlet weak var waterCountLabel: UILabel!
     @IBOutlet weak var waterMinusButton: UIButton!
     @IBOutlet weak var waterPlusButton: UIButton!
     
+    ///IBOutlet Collection
     @IBOutlet var componentViewCollection: [UIView]!
     @IBOutlet var titleLabelCollection: [UILabel]!
+    @IBOutlet var unitLabelCollection: [UILabel]!
+    @IBOutlet var resultLabelCollection: [UILabel]!
     
     // MARK: - VC Life Cycle
     
@@ -60,15 +86,15 @@ final class KmHomeViewController: UIViewController {
 
 extension KmHomeViewController {
     private func setViewUI() {
-        componentViewCollection.forEach {
-            $0.layer.cornerRadius = 20
-        }
+        componentViewCollection.forEach { $0.layer.cornerRadius = 20 }
         sleepBarView.layer.cornerRadius = 5
     }
     
     private func setLabelUI() {
-        titleLabelCollection.forEach {
-            $0.font = .NotoSans(.medium, size: 15)
-        }
+        titleLabelCollection.forEach { $0.font = .NotoSans(.medium, size: 15) }
+        unitLabelCollection.forEach { $0.font = .SshFontB2 }
+        resultLabelCollection.forEach { $0.font = .SshFontH1 }
+        [walkPercentLabel, sleepTimeLabel].forEach { $0?.font = .SshFontC1 }
+        [activityWalkLabel, activityHourLabel ,activityCalorieLabel].forEach { $0?.font = .SshFontB1 }
     }
 }

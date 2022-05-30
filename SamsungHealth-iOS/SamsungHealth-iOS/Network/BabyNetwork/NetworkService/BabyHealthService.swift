e0a2ef3ab7807330cfd8f4cc32836957ceb19539
Choi YoungLyn <joeuni0129@gmail.com>
2022-05-30 15:04:24 +0900

    [#34] 전체 건강 조회 서버 연결

--- SamsungHealth-iOS/SamsungHealth-iOS/Network/BabyNetwork/NetworkService/BabyHealthService.swift
+++ SamsungHealth-iOS/SamsungHealth-iOS/Network/BabyNetwork/NetworkService/BabyHealthService.swift
@@ -6,3 +6,48 @@
 //
 
 import Foundation
+import Alamofire
+
+struct BabyHealthService {
+    static let shared = BabyHealthService()
+    private init() {}
+    
+    func loadHealthData(completion: @escaping (NetworkResult<Any>) -> (Void))
+    {
+        let url = URLConstant.health
+        let header: HTTPHeaders = ["Content-Type" : "application/json"]
+        let dataRequest = AF.request(url,
+                                     method: .get,
+                                     encoding: URLEncoding.default,
+                                     headers: header)
+        
+        dataRequest.responseData { response in
+            switch response.result {
+            case .success:
+                guard let statusCode = response.response?.statusCode else { return }
+                guard let value = response.value else { return }
+                let networkResult = self.judgeStatus(by: statusCode, value)
+                completion(networkResult)
+            case .failure:
+                completion(.networkFail)
+            }
+        }
+    }
+    
+    private func judgeStatus (by statusCode: Int, _ data: Data) -> NetworkResult<Any> {
+        print(statusCode)
+        switch statusCode {
+        case 200: return isValidHealthURL(data: data)
+        case 500: return .serverErr
+        default: return .networkFail
+        }
+    }
+    
+    private func isValidHealthURL(data: Data) -> NetworkResult<Any> {
+        let decoder = JSONDecoder()
+        guard let decodedData = try? decoder.decode(BabyHealthResponseModel.self, from: data)
+        else { return .pathErr }
+        
+        return .success(decodedData.data as Any)
+    }
+}
--- SamsungHealth-iOS/SamsungHealth-iOS/Scene(baby)/Home/BabyHomeViewController.swift
+++ SamsungHealth-iOS/SamsungHealth-iOS/Scene(baby)/Home/BabyHomeViewController.swift
@@ -12,16 +12,37 @@ class BabyHomeViewController: UIViewController {
     // MARK: - @IBOutlet
     @IBOutlet var titleLabel: [UILabel]!
     @IBOutlet var daliyActivityLabel: [UILabel]!
+    //step
+    
+    @IBOutlet weak var stepCountLabel: UILabel!
+    @IBOutlet weak var stepTargetLabel: UILabel!
+    @IBOutlet weak var stepPercentLabel: UILabel!
+    @IBOutlet weak var stepCount_1Label: UILabel!
+    @IBOutlet weak var stepTimeLabel: UILabel!
+    @IBOutlet weak var stepActivityLabel: UILabel!
+    //calorie
+    @IBOutlet weak var calorieIntakeLabel: UILabel!
+    @IBOutlet weak var calorieTargetLabel: UILabel!
+    //sleep
+    @IBOutlet weak var sleepHourLabel: UILabel!
+    @IBOutlet weak var sleepMinuteLabel: UILabel!
+    @IBOutlet weak var sleepTimeLabel: UILabel!
+    //weight
     @IBOutlet weak var weightLabel: UILabel!
+    //water
+    @IBOutlet weak var waterLabel: UILabel!
     @IBOutlet weak var excerciseRecordButton: UIButton!
     @IBOutlet var bigLabel: [UILabel]!
     @IBOutlet var smallLabel: [UILabel]!
     @IBOutlet var detailLabel: [UILabel]!
     
+    
+    
     // MARK: - VC Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setFont()
+        loadHealthData()
     }
     
     // MARK: - Functions
@@ -50,3 +71,33 @@ extension BabyHomeViewController: BabyWeightViewContollerDelegate {
         weightLabel.text = String(weight)
     }
 }
+
+extension BabyHomeViewController {
+
+    func loadHealthData() {
+        BabyHealthService.shared.loadHealthData() {
+            response in
+            switch response {
+            case .success(let data):
+                guard let data = data as? BabyHealthDataClass else { return }
+                self.stepCountLabel.text = "\(data.step.count)"
+                self.stepTargetLabel.text = "/\(data.step.target)"
+                self.stepPercentLabel.text = "\(data.step.percent)%"
+                self.stepCount_1Label.text = "\(data.step.count)"
+                self.stepTimeLabel.text = "\(data.step.time)"
+                self.stepActivityLabel.text = "\(data.step.activity)"
+                self.calorieIntakeLabel.text = "\(data.calorie.intake)"
+                self.calorieTargetLabel.text = "/\(data.calorie.target)kcal"
+                self.sleepHourLabel.text = "\(data.sleep.hour)"
+                self.sleepMinuteLabel.text = "\(data.sleep.minute)"
+                self.sleepTimeLabel.text = data.sleep.time
+                self.weightLabel.text = "\(data.weight)"
+                self.waterLabel.text = "\(data.water)"
+                print(data)
+            default:
+                print(response)
+            }
+        }
+    }
+}
+

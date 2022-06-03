170210289a7b452dd7293b1823be57a16fd12e83
meenyweeny <74659491+meenyweeny@users.noreply.github.com>
2022-06-03 21:43:29 +0900

    [X] baseurl 변경사항 반영

--- SamsungHealth-iOS/SamsungHealth-iOS/Network/Base/URLConstant.swift
+++ SamsungHealth-iOS/SamsungHealth-iOS/Network/Base/URLConstant.swift
@@ -11,7 +11,7 @@ struct URLConstant {
     
     // MARK: - base URL
     
-    static let baseURL = "http://43.200.4.231:8000"
+    static let baseURL = "http://15.165.213.118:8000"
     
     // MARK: - Health
     
